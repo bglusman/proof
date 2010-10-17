636cca01e318ed314b2e5ae3fad061a30560fb6a
Stuart Ellis <stuart@stuartellis.eu>
2010-10-17 15:15:30 +0100

    Added comment to explain how to generate summaries.

--- lib/proof/content/summary.rb
+++ lib/proof/content/summary.rb
@@ -3,7 +3,8 @@
 module Proof
   module Content
   
-    # Summary of a file
+    # Summary of a file.
+    # Use Proof::Content::Analyzer to generate summaries
     class Summary
       attr_accessor :filename, :readability
     
